5eb4ba7ad5c368eee1a25b53b53141bd3504fc5a
rsaroha <rsaroha@gmail.com>
2025-04-19 19:25:32 -0400

    Runtime error fixed

--- tests/test.py
+++ tests/test.py
@@ -1,4 +1,5 @@
 import wiringpi
-io = wiringpi.GPIO(wiringpi.GPIO.WPI_MODE_PINS)
-print io.digitalRead(1)
-print io.analogRead(1)
+
+io = wiringpi.GPIO(wiringpi.GPIO.WPI_MODE_GPIO)
+print (io.digitalRead(1))
+print (io.analogRead(1))
